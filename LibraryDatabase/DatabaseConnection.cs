66be930eeac6e485ca66b69cc9191022673671a4
Forkxel <pavel.halik06@gmail.com>
2025-12-29 20:28:31 +0100

    Added class for Member table.

--- LibraryDatabase/DatabaseConnection.cs
+++ LibraryDatabase/DatabaseConnection.cs
@@ -1,6 +1,25 @@
-﻿namespace LibraryDatabase;
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LibraryDatabase;
 
 public class DatabaseConnection
 {
-    
+    public static SqlConnection GetConnection()
+    {
+        try
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["LibraryDb"].ConnectionString;
+            
+            var connection = new SqlConnection(connectionString);
+            
+            connection.Open();
+            return connection;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error connecting to database" + e.Message);
+        }
+        return null;
+    }
 }
